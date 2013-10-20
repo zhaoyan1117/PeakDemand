b58e886e11023676961f0c7b6c8a9ddb468f5643
Yan Zhao <zhaoyan1117@gmail.com>
2013-10-19 18:50:00 -0700

    init commit, cucumber rspec setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeekDemand::Application.config.secret_token = '273356532e8921d294b00d0ee7043ea1478072a42f1ba5b94ae06f8ecb4b46c641d97a18f1c736d1db01fba232fa3c2dc768b0b0b0405155712573b49bbaaad9'
