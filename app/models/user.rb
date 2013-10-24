87d7c2f950598f8107808c626376d8661f0d3136
Yan Zhao <zhaoyan1117@gmail.com>
2013-10-23 21:49:46 -0700

    Add more validations for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,12 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me 
   attr_accessible :name, :is_provider, :is_consumer, :is_administrator
 
-  has_many :resources
+  has_many :resources, :class_name => 'Resource', :foreign_key => 'provider_id'
+  has_many :demands, :class_name => 'Demand', :foreign_key => 'consumer_id'
+
   validate :validate_must_have_identity
+  validate :validate_only_provider_can_create_resources
+  validate :validate_only_consumer_can_create_demands
   validates :email, :uniqueness => true
 
   def identity
@@ -24,6 +28,19 @@ class User < ActiveRecord::Base
     if !is_provider && !is_consumer
       errors[:base] << "must have an identity!"
     end
+  end
+
+  def validate_only_provider_can_create_resources
+    if !is_provider && !resources.empty?
+      errors[:base] << "only provider can create resources!"
+    end
+  end
+
+  def validate_only_consumer_can_create_demands
+    if !is_consumer && !demands.empty?
+      errors[:base] << "only consumers can create demands!"
+    end
+
   end
 
 
