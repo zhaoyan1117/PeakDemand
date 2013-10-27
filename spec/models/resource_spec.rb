6868bf22bd80ce2e51c68b2c1c4d207bbdf5e9e1
Yan Zhao <zhaoyan1117@gmail.com>
2013-10-26 18:29:34 -0700

    Rspec tests for resource model and user model. --Betty

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -2,25 +2,65 @@ require 'spec_helper'
 
 describe Resource do
 
-  it "should have a name"
+  before :each do
+    @provider = mock_model(User)
 
-  it "should have a start date"
+    @r = Resource.new(
+      :name => "sample_resource",
+      :start_at => Date.today,
+      :end_at => Date.tomorrow,
+      :provider => @provider,
+      :description => "description"
+      )
+  end
 
-  it "should have an end date"
+  it "should have a name" do
+    @r.name = nil
+    @r.valid?.should be_false
+    @r.errors.messages.should include(:name)
+  end
 
-  it "should belong to an provider"
+  it "should have a start date" do
+    @r.start_at = nil
+    @r.valid?.should be_false
+    @r.errors.messages.should include(:start_at)
+    @r.errors.messages.should include(:date_range)
+  end
 
-  it "should not have a start date later than its end date"
+  it "should have an end date" do
+    @r.end_at = nil
+    @r.valid?.should be_false
+    @r.errors.messages.should include(:end_at)
+    @r.errors.messages.should include(:date_range)
+  end
+
+  it "should belong to an provider" do
+    @r.provider = nil
+    @r.valid?.should be_false
+    @r.errors.messages.should include(:provider)
+  end
+
+  it "should not have a start date later than its end date" do
+    @r.end_at = Date.yesterday.yesterday
+    @r.valid?.should be_false
+    @r.errors.messages.should include(:date_range)
+  end
 
   describe "provider_name" do
 
-    it "should return the provider name"
+    it "should return the provider name" do
+      @provider.should_receive(:name)
+      @r.provider_name
+    end
 
   end
 
   describe "get_number_of_demands" do
 
-    it "should return the number of demands this resource has"
+    it "should return the number of demands this resource has" do
+      @r.demands.should_receive(:size)
+      @r.get_number_of_demands
+    end
 
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,45 @@ require 'spec_helper'
 
 describe User do
 
-  it "should have an unique email address"
+  before :each do
+    @u1 = User.create!(:name => "u1", 
+                       :email => "test@test.com", 
+                       :password => "u1password",
+                       :password => "u1password",
+                       :is_provider => true
+                      )
+  end
 
-  it "should have an identity"
+  it "should have an unique email address" do
+    u2 = User.new(:name => "u2", 
+                  :email => "test@test.com", 
+                  :password => "u2password",
+                  :password => "u2password",
+                  :is_provider => true
+                 )
 
-  it "should be able to create resources only if it is a provider"
+    u2.valid?.should be_false
+    u2.errors.messages.should include(:email)
+  end
 
-  it "should be able to create demands only if it is a consumer"
+  it "should have an identity" do
+    @u1.is_provider = nil
+    @u1.is_consumer = nil
+    @u1.valid?.should be_false
+    @u1.errors.messages.should include(:identity)
+  end
 
   describe "identity" do
-    it "should return the identity of the user"
+    it "should return the identity of the user" do
+      @u1.is_consumer = true
+      @u1.is_provider = true
+      @u1.identity.should include("Provider")
+      @u1.identity.should include("Consumer")
+    end
+  end
+
+  after :each do
+    @u1.destroy rescue true
   end
 
 end
