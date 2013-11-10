4ff448f7da838243b3b4a7187dbb3c9fed4b890e
Yan Zhao <zhaoyan1117@gmail.com>
2013-11-09 22:17:05 -0800

    Created after_create filter in resource module to create calendars.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -9,6 +9,8 @@ class Resource < ActiveRecord::Base
   validates :name, :provider, :start_at, :presence => true
   include DateRangeValidation
 
+  after_create :create_calendar
+
   def provider_name
     provider.name
   end
@@ -17,5 +19,58 @@ class Resource < ActiveRecord::Base
     demands.size
   end
 
-end
+  def get_url
+    cals = [occupy_cal_id, heavy_cal_id, moderate_cal_id, light_cal_id]
 
+    colors = {
+      occupy_cal_id => '#A32929',
+      heavy_cal_id => '#7A367A',
+      moderate_cal_id => '#AB8B00',
+      light_cal_id => '#528800'
+    }
+
+    start = start_at.strftime '%Y%m%d'
+    done = end_at.strftime '%Y%m%d'
+    params = {
+      dates: [start, done].join("/"), 
+      showTitle: '0',
+      showNav: '0',
+      showPrint: '0',
+      showTabs: '0',
+      showCalendars: '0',
+      showTz: '0',
+      ctz: 'America/Los_Angeles'
+    }
+    
+    GCal4Ruby::Service.new.to_iframe cals, params, colors
+  end
+
+  private
+
+  def create_calendar
+    service = get_gcal_service
+
+    light_calendar = GCal4Ruby::Calendar.new(service, :title => "#{name}_light", :public => true)
+    light_calendar.save
+    self.light_cal_id = light_calendar.id and save!
+
+    moderate_calendar = GCal4Ruby::Calendar.new(service, :title => "#{name}_moderate", :public => true)
+    moderate_calendar.save
+    self.moderate_cal_id = moderate_calendar.id and save!
+
+    heavy_calendar = GCal4Ruby::Calendar.new(service, :title => "#{name}_heavy", :public => true)
+    heavy_calendar.save
+    self.heavy_cal_id = heavy_calendar.id and save!
+
+    occupy_calendar = GCal4Ruby::Calendar.new(service, :title => "#{name}_occupy", :public => true)
+    occupy_calendar.save
+    self.occupy_cal_id = occupy_calendar.id and save!
+  end
+
+  def get_gcal_service
+    service = GCal4Ruby::Service.new
+    service.authenticate
+    service
+  end
+
+end
