1ac3f99e0375da2b510f22ba2240ec99ee10fa0b
Beidi Chen <bettychen824@gmail.com>
2013-12-09 19:05:05 -0800

    Email 80% done.

--- app/controllers/user_mailer_controller.rb
+++ app/controllers/user_mailer_controller.rb
@@ -7,10 +7,13 @@ class UserMailerController < ApplicationController
   	demand = Demand.find_by_id params[:mail][:demand_id]
   	url = resource_url(:id=>demand.resource.id)
     m = UserMailer.send_to_consumer(@user, params[:mail], demand, url).deliver
-    flash[:notice] = "email sent!"
-    redirect_to '/'
+    render :json => {:msg => 'Your email has been sent successfully!'}
   end
 
   def send_email_provider
+    resource = Resource.find_by_id params[:mail][:resource_id]
+    url = resource_url(:id=>resource.id)
+    m = UserMailer.send_to_provider(@user, params[:mail], resource, url).deliver
+    render :json => {:msg => 'Your email has been sent successfully!'}
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,8 +12,27 @@ class UserMailer < ActionMailer::Base
 
     mail(:reply_to  => @sender_email, 
     	:to => mail[:to], 
+      :cc => @sender_email,
     	:subject => "[PeakDemand] about #{@resource_name}",) do |format|
-        format.html {render "generate_email"}
+        format.html {render "generate_email_to_concumer"}
       end
   end
+
+  def send_to_provider(user, mail, resource, resource_url)
+    @body = mail[:body]
+    @sender_name = user.name
+    @sender_email = user.email
+    @demand_start = resource.start_at
+    @demand_end = resource.end_at
+    @resource_name = resource.name
+    @resource_link = resource_url
+
+    mail(:reply_to  => @sender_email, 
+      :to => mail[:to], 
+      :cc => @sender_email,
+      :subject => "[PeakDemand] about #{@resource_name}",) do |format|
+        format.html {render "generate_email_to_provider"}
+      end
+  end
+
 end
